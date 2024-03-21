9c80632fd99382e5ace0591229c2c762f19c5b1f
LE DINH THANH <thanhldse160337@fpt.edu.vn>
2024-03-21 17:45:10 +0700

    nhat ngu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import DashBoard from './Components/DashBoard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './Components/AppLayout';
 
 function App() {
   return (
-    <DashBoard />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route path='/dashboard' element={<DashBoard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
 export default App;
+
+
+
